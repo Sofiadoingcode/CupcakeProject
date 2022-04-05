8c4b7a105184583f199bd2c19ea7675c008fd827
SMerved <90383268+SMerved@users.noreply.github.com>
2022-04-05 14:47:19 +0200

    jn

--- cupcakeProjektINTEL/src/main/java/dat/startcode/model/persistence/ConnectionPool.java
+++ cupcakeProjektINTEL/src/main/java/dat/startcode/model/persistence/ConnectionPool.java
@@ -12,8 +12,8 @@ public class ConnectionPool
 {
     private HikariDataSource ds;
     private static String USER = "root";
-    private static String PASSWORD = "1234";
-    private static String URL = "jdbc:mysql://localhost:3306/cupcakedatabase";
+    private static String PASSWORD = "";
+    private static String URL = "jdbc:mysql://localhost:3306/cupcakeDatabase";
 
     public ConnectionPool()
     {
